dbd640615dc639a9e3bd2885d317bf77e0af4629
blazinchicken <kyblazinchicken@gmail.com>
2025-09-09 10:54:15 -0500

    mid class color swatching

--- day2.py
+++ day2.py
@@ -1,12 +1,17 @@
 from PIL import Image
 
-image = Image.open("./lizard.jpg")
+image = Image.open("./butterfly.jpg")
 data = image.load()
 
 for x in range(image.width):
-    for y in range(image.width):
+    for y in range(image.height):
         pixel = data[x,y]
 
-
-        data[x,y] = pixel
-image.save("./lizard.png")
+        r = pixel[0]
+        g = pixel[1]
+        b = pixel[2]
+        if (g < 120):
+            k = .33*r + .33*g + .33*b
+            k = int(k)
+            data[x,y] = (k,k,k)
+image.save("./butterfly.png")

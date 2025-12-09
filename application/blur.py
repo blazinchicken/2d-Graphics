fa25b09e7c6b614e0fe9e9c56b6b419a0a18224f
blazinchicken <kyblazinchicken@gmail.com>
2025-12-09 11:06:48 -0600

    blur plans

--- application/blur.py
+++ application/blur.py
@@ -12,7 +12,16 @@ GOAL: make multiple blur functions that change the image by a bit at a time, all
     this would be extremely expensive in terms of time but would work.
 
 2. interpolation
-    - This one seems a little faster in that 
+    - This one seems a little faster in that it is a closest neighbor function that doesnt act in a simple matrix, would apply this for any real changes from the main one.
+    
+3. spiral? 
+    - This one is a different thing, and could be much harder to implement, but the goal would be to find points on the base image, write to the base image the blur points
+    that are from the middle point of the image, and then proceed in a spiral so that it will be able to continue to look like a full spiral.
+        - I am not sure that this is actively how I would go about doing this, but overall its odd. 
 """
 image = Image.open("./wave.jpg")
 raster = image.load()
+
+for y in range(image.height):
+    for x in range(image.width):
+        
